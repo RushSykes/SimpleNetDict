94ca55a4575dabec844e17a46fdc26414703896a
RushSykes <thedoraemon@outlook.com>
2016-11-17 20:01:32 +0800

    Connector for database setup

--- src/Dict/Connector/DBConnector.java
+++ src/Dict/Connector/DBConnector.java
@@ -1,8 +1,93 @@
 package Dict.Connector;
 
+import java.sql.*;
+
 /*
  * This is the connector class used for C/S mode
  * Connecting to database
  */
 public class DBConnector {
+    private String DB_Driver;
+    private String DB_Url;
+    private String DB_User;
+    private String DB_Password;
+    private Connection DB_Conn;
+    private boolean connDone;
+
+    // =========================================
+    // Constructor
+    public DBConnector() {
+        DB_Driver = "com.mysql.jdbc.Driver";
+        DB_Url = "jdbc:mysql://localhost:3306/dictdatabase";
+        DB_User = "Rush";
+        DB_Password = "supersonic123";
+        DB_Conn = null;
+        connDone = false;
+    }
+
+    // =========================================
+    // Create connection
+    public void connect() {
+        // Register driver
+        try {
+            Class.forName(DB_Driver);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver loading failed");
+            e.printStackTrace();
+        }
+
+        // Create connection
+        try {
+            DB_Conn = DriverManager.getConnection(DB_Url, DB_User, DB_Password);
+            connDone = true;
+        } catch (SQLException ex) {
+            // Handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    // Get connection stat for top-level code parts
+    public boolean getConnStat() {
+        if(connDone)
+            return true;
+        else
+            return false;
+    }
+
+    // =========================================
+    // Deal with user data in db
+    // Return :
+    // -1 for exception
+    // 0 for success
+    // 1 for wrong password
+    // 2 for username does not exist
+    public int findUser_Login(String userName, String password) {
+        // Make sure the connection is established
+        if(connDone) {
+            try {
+                Statement stmt = DB_Conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE userName =\'" + userName + "\' ");
+                while(rs.next()) {
+                    if(rs.getString("password").equals(password)) {
+                        return 0;
+                    }
+                    else
+                        return 1;
+                }
+                return 2;
+            }
+            catch(SQLException ex) {
+                // Handle any errors
+                System.out.println("Find user");
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+                return -1;
+            }
+        } // Connection established
+        System.out.println("Connection not established");
+        return -1;
+    }
 }
--- src/Dict/UI/DictFrame.java
+++ src/Dict/UI/DictFrame.java
@@ -1,5 +1,6 @@
 package Dict.UI;
 
+import Dict.Connector.DBConnector;
 import javafx.application.Application;
 import javafx.event.*;
 import javafx.scene.*;
@@ -40,12 +41,16 @@ public class DictFrame extends Application {
     Button registerButton, backButton;
     final Text regInfo = new Text();
 
+    // Connector
+    DBConnector connector = new DBConnector();
+
     @Override
     public void start(Stage primaryStage) {
         /* Set properties of the window */
         primaryStage.setTitle("NeT Dictionary");
 
         /* Set up welcome Interface */
+        // ==============================================================
         // welcomePane
         welcomePane = new GridPane();
         welcomePane.setAlignment(Pos.CENTER);
@@ -76,15 +81,120 @@ public class DictFrame extends Application {
         welcomeButtonHBox = new HBox(10);
         signUpButton = new Button("Sign Up");
         logInButton = new Button("Log In");
+
+        welcomeButtonHBox.setAlignment(Pos.BOTTOM_RIGHT);
+        welcomeButtonHBox.getChildren().addAll(signUpButton, logInButton);
+        welcomePane.add(welcomeButtonHBox, 1, 4);
+
+        welcomeScene = new Scene(welcomePane, 480, 640);
+
+        // ===============================================================
+        // SignUp scene
+        signUpPane = new GridPane();
+        signUpPane.setAlignment(Pos.CENTER);
+        signUpPane.setHgap(10);
+        signUpPane.setVgap(10);
+        signUpPane.setPadding(new Insets(25, 25, 25, 25));
+
+        // signUpTitle
+        signUpTitle = new Text("Sign Up\nHere");
+        signUpTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        signUpPane.add(signUpTitle, 0, 0, 2, 1);
+
+        /*
+          Label newUser, newPassword, newConfirm;
+          TextField newUserTextField;
+          PasswordField newPasswordField, confirmField;
+          HBox regButtonHBox;
+          Button registerButton, backButton;
+          final Text regInfo = new Text();
+        */
+        // New User name and password
+        newUser = new Label("New user Name:");
+        signUpPane.add(newUser, 0, 1);
+
+        newUserTextField = new TextField();
+        signUpPane.add(newUserTextField, 1, 1);
+
+        newPassword = new Label("Password:");
+        signUpPane.add(newPassword, 0, 2);
+
+        newPasswordField = new PasswordField();
+        signUpPane.add(newPasswordField, 1, 2);
+
+        newConfirm = new Label("Confirm password:");
+        signUpPane.add(newConfirm, 0, 3);
+
+        confirmField = new PasswordField();
+        signUpPane.add(confirmField, 1, 3);
+
+        regButtonHBox = new HBox(10);
+        backButton = new Button("Back");
+        registerButton = new Button("Register!");
+
+        regButtonHBox.setAlignment(Pos.BOTTOM_RIGHT);
+        regButtonHBox.getChildren().addAll(backButton, registerButton);
+        signUpPane.add(regButtonHBox, 1, 5);
+
+        signUpScene = new Scene(signUpPane, 480, 640);
+
+        // =============================================================
+        // Connector
+        connector.connect();
+
+        // =============================================================
+        // Listener and handlers
+
+        // When click login, perform signIn process
         logInButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 // Should look up the username in the database first
                 // To be added...
-                userNotExist.setFill(Color.FIREBRICK);
-                userNotExist.setText("User doesn't exist");
+
+                // If connection is valid
+                if(connector.getConnStat()) {
+                    int flag = connector.findUser_Login(userTextField.getText(), passwordField.getText());
+                    switch(flag) {
+                        case 0:
+                            break;
+                        case 1:
+                            userNotExist.setFill(Color.FIREBRICK);
+                            userNotExist.setText("Wrong password");
+                            break;
+                        case 2:
+                            userNotExist.setFill(Color.FIREBRICK);
+                            userNotExist.setText("User doesn't exist");
+                            break;
+                        default:
+                    }
+                }
             }
         });
+
+        // SignUp scene buttons below
+        // When click back, go back to welcome scene
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // First clear all the fields
+                newUserTextField.clear();
+                newPasswordField.clear();
+                confirmField.clear();
+                primaryStage.setScene(welcomeScene);
+            }
+        });
+
+        // When click register, perform the register process
+        registerButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Should look up the username in the database first
+                // To be added...
+            }
+        });
+
+        // When click signUp, switch to another scene and prompt for more info
         signUpButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -92,79 +202,11 @@ public class DictFrame extends Application {
                 userTextField.clear();
                 passwordField.clear();
                 userNotExist.setText("");
-                // Switch to the signUpScene
-                // SignUpPane
-                signUpPane = new GridPane();
-                signUpPane.setAlignment(Pos.CENTER);
-                signUpPane.setHgap(10);
-                signUpPane.setVgap(10);
-                signUpPane.setPadding(new Insets(25, 25, 25, 25));
-
-                // signUpTitle
-                signUpTitle = new Text("Sign Up\nHere");
-                signUpTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-                signUpPane.add(signUpTitle, 0, 0, 2, 1);
-
-                /*
-                    Label newUser, newPassword, newConfirm;
-                    TextField newUserTextField;
-                    PasswordField newPasswordField, confirmField;
-                    HBox regButtonHBox;
-                    Button registerButton, backButton;
-                    final Text regInfo = new Text();
-                 */
-                // New User name and password
-                newUser = new Label("New user Name:");
-                signUpPane.add(newUser, 0, 1);
-
-                newUserTextField = new TextField();
-                signUpPane.add(newUserTextField, 1, 1);
-
-                newPassword = new Label("Password:");
-                signUpPane.add(newPassword, 0, 2);
-
-                newPasswordField = new PasswordField();
-                signUpPane.add(newPasswordField, 1, 2);
-
-                newConfirm = new Label("Confirm password:");
-                signUpPane.add(newConfirm, 0, 3);
-
-                confirmField = new PasswordField();
-                signUpPane.add(confirmField, 1, 3);
-
-                regButtonHBox = new HBox(10);
-                backButton = new Button("Back");
-                registerButton = new Button("Register!");
-
-                // WHen click back, go back to welcome scene
-                backButton.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        // First clear all teh fields
-                        newUserTextField.clear();
-                        newPasswordField.clear();
-                        confirmField.clear();
-                        primaryStage.setScene(welcomeScene);
-                    }
-                });
-
-                regButtonHBox.setAlignment(Pos.BOTTOM_RIGHT);
-                regButtonHBox.getChildren().addAll(backButton, registerButton);
-                signUpPane.add(regButtonHBox, 1, 5);
-
-                signUpScene = new Scene(signUpPane, 480, 640);
 
                 // Assign it to the stage, whilst cancel teh assignment of welcome scene
                 primaryStage.setScene(signUpScene);
-
             }
         });
-
-        welcomeButtonHBox.setAlignment(Pos.BOTTOM_RIGHT);
-        welcomeButtonHBox.getChildren().addAll(signUpButton, logInButton);
-        welcomePane.add(welcomeButtonHBox, 1, 4);
-
-        welcomeScene = new Scene(welcomePane, 480, 640);
 
         // Assign the scene to the stage
         primaryStage.setScene(welcomeScene);
